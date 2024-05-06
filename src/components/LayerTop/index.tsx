e1231be9f8ba633f26a571826bd1ef953c27e4ad
ryu <ryuichiiha@gmail.com>
2024-05-06 11:54:52 +0900

    規則に沿った形に直す

---
+++ src/components/LayerTop/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+import styled from "styled-components";
+
+export function LayerTop() {
+  return (
+    <LayerElement>
+      <Rooftop />
+      <Floor />
+    </LayerElement>
+  );
+}
+
+const LayerElement = styled.div`
+  position: relative;
+
+  width: 100%;
+  height: 100%;
+`;
+
+const Rooftop = styled.div`
+  background-color: #f38484;
+
+  width: 100%;
+  height: 56px;
+
+  position: absolute;
+  top: 0px;
+`;
+
+const Floor = styled.div`
+  background-color: #724a4a;
+
+  width: 100%;
+  height: 32px;
+
+  position: absolute;
+  bottom: 0px;
+`;
